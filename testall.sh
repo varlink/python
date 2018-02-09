0149c40b04c6837ab07ed756701ce977c9f19199
Harald Hoyer <harald@redhat.com>
2018-02-09 15:08:12 +0100

    add IPv4 tests

--- testall.sh
+++ testall.sh
@@ -19,7 +19,18 @@ sleep 1
 ./client-simple-stop.py unix:@test
 wait %1
 
-# test ip:
+# test ip: IPv4
+./server-simple.py 'ip:0.0.0.0:12345' &
+sleep 1
+./client-simple-stop.py 'ip:127.0.0.1:12345'
+wait
+
+./server-twisted.py 'ip:0.0.0.0:12345' &
+sleep 1
+./client-simple-stop.py 'ip:127.0.0.1:12345'
+wait
+
+# test ip: IPv6
 ./server-simple.py 'ip:[::1]:12345' &
 sleep 1
 ./client-simple-stop.py 'ip:[::1]:12345'
