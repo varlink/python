0285e4f7c2a4680a202c5b894b5051f76e32a0bf
Harald Hoyer <harald@hoyer.xyz>
2018-02-28 11:35:10 +0100

    Update testall.sh

--- testall.sh
+++ testall.sh
@@ -6,35 +6,35 @@ cp examples/* .
 
 # test unix:
 ./server-simple.py unix:@varlink$$ &
-sleep 1
+sleep 2
 ./client-simple-stop.py unix:@varlink$$
 wait %1
 
 ./server-twisted.py unix:@varlink$$ &
-sleep 1
+sleep 2
 ./client-simple-stop.py unix:@varlink$$
 wait %1
 
 # test tcp: IPv4
 ./server-simple.py 'tcp:0.0.0.0:25645' &
-sleep 1
+sleep 2
 ./client-simple-stop.py 'tcp:127.0.0.1:25645'
 wait
 
 ./server-twisted.py 'tcp:0.0.0.0:25645' &
-sleep 1
+sleep 2
 ./client-simple-stop.py 'tcp:127.0.0.1:25645'
 wait
 
 if ! [[ $TRAVIS ]]; then
     # test tcp: IPv6
     ./server-simple.py 'tcp:[::1]:25645' &
-    sleep 1
+    sleep 2
     ./client-simple-stop.py 'tcp:[::1]:25645'
     wait
 
     ./server-twisted.py 'tcp:[::1]:25645' &
-    sleep 1
+    sleep 2
     ./client-simple-stop.py 'tcp:[::1]:25645'
     wait
 fi
