907f44dede838ee9ad040a31af95d0c0bf278ca1
Harald Hoyer <harald@redhat.com>
2018-03-21 15:13:22 +0100

    add complex test

--- varlink/tests/test_scanner.py
+++ varlink/tests/test_scanner.py
@@ -34,3 +34,29 @@ error ActionFailed (reason: string)
     assert interface.get_method("StopServing") != None
     assert isinstance(interface.members.get("ActionFailed"), varlink._Error)
     assert isinstance(interface.members.get("State"), varlink._Alias)
+
+
+def test_complex():
+    interface = varlink.Interface("""
+interface org.example.complex
+
+type TypeEnum ( a, b, c )
+
+type TypeFoo (
+    bool: bool,
+    int: int,
+    float: float,
+    string: string,
+    enum: ( foo, bar, baz )[],
+    type: TypeEnum,
+    anon: ( foo: bool, bar: int, baz: (a: int, b: int)[] )
+)
+
+method Foo(a: (b: bool, c: int), foo: TypeFoo) -> (a: (b: bool, c: int)[], foo: TypeFoo)
+
+error ErrorFoo (a: (b: bool, c: int), foo: TypeFoo)
+""")
+    assert interface.name == "org.example.complex"
+    assert interface.get_method("Foo") != None
+    assert isinstance(interface.members.get("ErrorFoo"), varlink._Error)
+    assert isinstance(interface.members.get("TypeEnum"), varlink._Alias)
