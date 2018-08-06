162a1ea434d303854862b72a79de3e9c2b64720d
Harald Hoyer <harald@redhat.com>
2018-08-06 10:27:06 +0200

    increase version

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
 setup(
-    name = "varlink",
-    packages = ["varlink"],
-    version = "27.1.0",
-    description = "Varlink",
-    long_description = "Python implementation of the varlink protocol http://varlink.org",
-    author = "Lars Karlitski<lars@karlitski.net>, Harald Hoyer<harald@redhat.com>",
-    author_email = "harald@redhat.com",
-    url = "https://github.com/varlink/python",
-    license = "ASL 2.0",
-    keywords = "ipc varlink rpc",
-    python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    package_data = {
-        "varlink": [ "*.varlink" ]
+    name="varlink",
+    packages=["varlink"],
+    version="27.1.1",
+    description="Varlink",
+    long_description="Python implementation of the varlink protocol http://varlink.org",
+    author="Lars Karlitski<lars@karlitski.net>, Harald Hoyer<harald@redhat.com>",
+    author_email="harald@redhat.com",
+    url="https://github.com/varlink/python",
+    license="ASL 2.0",
+    keywords="ipc varlink rpc",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    package_data={
+        "varlink": ["*.varlink"]
     },
-    classifiers = [
+    classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
