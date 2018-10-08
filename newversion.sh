6eb32ea0daf28d0b000d6abb6bee4902876c991a
Harald Hoyer <harald@redhat.com>
2018-10-08 16:20:09 +0200

    newversion.sh: use curl

--- newversion.sh
+++ newversion.sh
@@ -20,7 +20,7 @@ rm -fr dist
 python3 setup.py bdist_wheel --universal
 python3 setup.py sdist
 twine upload --skip-existing --sign-with gpg2 -s dist/*
-wget https://github.com/varlink/python-varlink/archive/${version}/python-varlink-${version}.tar.gz
+curl -L -O https://github.com/varlink/python-varlink/archive/${version}/python-varlink-${version}.tar.gz
 rm -fr docs/build
 python3 setup.py build_sphinx --source-dir=docs/ --build-dir=docs/build --all-files
 GIT_DEPLOY_DIR=$(pwd)/docs/build/html GIT_DEPLOY_BRANCH=gh-pages ./git-deploy-branch.sh -m "doc update"
