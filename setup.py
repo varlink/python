30da660d616c0e241b7fa66ce02bb77cfe9d35ad
Harald Hoyer <harald@hoyer.xyz>
2021-12-06 13:25:33 +0100

    ci(TestPyPi): Enables a version format
    .. so that upload to TestPyPI is successful
    
    Signed-off-by: Harald Hoyer <harald@hoyer.xyz>

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+def local_scheme(_):
+    """Enables a version format so that upload to TestPyPI is successful.
+     For example: 2.6.2.dev8
+     See https://github.com/pypa/setuptools_scm/issues/342.
+     """
+    return ""
+
 setup(
-    use_scm_version=True,
+    use_scm_version={"local_scheme": local_scheme},
 )
